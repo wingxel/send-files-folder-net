55273ec9f7e5a94e08a549a318b54f31a7e2f1c7
wingxel <wingxel@gmail.com>
2023-05-16 12:33:34 +0300

    Added receiver_utils.py

---
+++ receiver_utils.py
@@ -0,0 +1,13 @@
+"""
+Receiver utility functions and consts
+"""
+
+import argparse
+
+
+def get_receiver_args() -> dict:
+    """
+    Get receiver commandline arguments
+    :return:
+    """
+    return {}
